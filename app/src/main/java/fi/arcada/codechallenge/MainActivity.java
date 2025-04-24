cecf18091d9c1b3c5c1a366e7f40260fe55594e2
salmeaar <salmeaar@arcada.fi>
2025-04-24 16:24:38 +0300

    Codechallenge 7

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -3,6 +3,7 @@ package fi.arcada.codechallenge;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,81 +12,43 @@ import android.content.SharedPreferences;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 
-// Denna MainActivity håller koll på hur många gånger man öppnat appen
-// Den säger även två olika meddelanden - En vid start och en efter man varit i settings
+
 public class MainActivity extends AppCompatActivity {
-    // En boolean för att hålla koll på om vi just starta appen
-    private boolean isInitialLaunch = true;
-    private TextView appCountTextView;
-    private TextView welcomeTextView;
-    private FloatingActionButton settingsButton;
 
-    // Vi gör en instans av SharedPreferences
-    private SharedPreferences sharedPreferences;
-    // Man kan ha många olika shared preferences
-    // En sharedPrefence är som cookies i webdev
-    // Man spara sen nyckel-värd par (lite som HashMaps från tidigare under lektionen)
-    // Vi gör en sharedPreferences för våra "counters" (räknare)
-    // Vi gör sen också vår "räknare"
-    private static final String PREFS_NAME = "AppCounterPrefs";
-    private static final String APP_COUNTER_KEY = "appCounter";
+    ArrayList<Double> values = new ArrayList<Double>();
 
-    @SuppressLint("MissingInflatedId")
+    private TextView myTextview;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstancesState){
+        super.onCreate(savedInstancesState);
         setContentView(R.layout.activity_main);
 
-        appCountTextView = findViewById(R.id.appCount);
-        welcomeTextView = findViewById(R.id.welcomeView);
+        myTextview = findViewById(R.id.myTextView);
 
-        // Vår settings button - Med snygg icon
-        settingsButton = findViewById(R.id.myFloatingActionButton);
 
-        // Här öppnar vi den specifika sharedPreferences filen vi definerat (PREFS_NAME dvs. AppCounterPrefs)
-        // Vi läser den sedan i privat läge (Default) så vår klass har tillgång till den
-        sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-
-        // En enkel intro text
-        welcomeTextView.setText("Hej på dig!");
-
-        // Hämta värdet som just nu finns i vår nyckel APP_COUNTER_KEY
-        int appCounter = sharedPreferences.getInt(APP_COUNTER_KEY, 0);
-
-        // Öka värdet (vi har ju just öppnat appen och denna onCreate() körs)
-        appCounter++;
-
-        // Spara det nya värdet med edit & apply
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putInt(APP_COUNTER_KEY, appCounter);
-        editor.apply();
-
-        // Vi visar vår fina counter i vår textView
-        appCountTextView.setText(String.valueOf(appCounter));
-
-        // Vår clickListener för settings
-        settingsButton.setOnClickListener(v -> openSettings());
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // onResume kallas varje gång denna Activity kommer i fokus
-        // Vi kan använda vår boolean för att se till att saker funkar när vi kommer från settings
-        if (!isInitialLaunch) {
-            welcomeTextView.setText("Hej igen!");
-        } else {
-            isInitialLaunch = false;
+        for (int i = 0; i<13; i++){
+            double randomValue = Math.random();
+            values.add(randomValue);
         }
+    }
+    public void buttonClickHandler(View view){calculate();}
 
+    private void calculate(){
+        Statistic meanCalc = new Statistic();
+
+        int windowSize = 3;
+
+        ArrayList<Double> temps = new ArrayList<>(Arrays.asList(17.5, 16.0, 16.5, 15.0, 17.5, 18.0, 15.5, 20.0, 19.5, 16.0));
+        ArrayList<Double> result = meanCalc.glideMeanValue(temps, windowSize);
+        myTextview.setText("SMA: "+result);
     }
 
-    private void openSettings() {
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
-    }
+
 }
 
 
